9e551e0b662211e1c4e0bd6a8e71c00225884a05
santiago <santimarin@utp.edu.co>
2016-09-30 12:57:31 -0500

    Commit inicial del proyecto de Django.

---
+++ ptti/psychologyTest/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.login_page, name="login_page" ),
+    url(r'^logout/', views.logout_page, name="logout_page" ),
+    url(r'^home/', views.home, name="home" ),
+    url(r'^task/', views.my_task, name="task" ),
+]
